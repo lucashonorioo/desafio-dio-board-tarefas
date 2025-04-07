d4aba2a54422c2081b7ff32d7c36d9466ad89e46
Lucas Honorio <lucashonorioo@hotmail.com>
2025-04-07 18:01:54 -0300

    Configurando Banco de dados MySql, criando primeira tabela

--- desafio-board/src/main/java/dio/board/DesafioBoardApplication.java
+++ desafio-board/src/main/java/dio/board/DesafioBoardApplication.java
@@ -1,13 +1,22 @@
 package dio.board;
 
+import dio.board.persistence.migration.MigrationStrategy;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+import java.sql.SQLException;
+
+import static dio.board.persistence.config.ConnectionConfig.getConnection;
+
+
 public class DesafioBoardApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(DesafioBoardApplication.class, args);
+	public static void main(String[] args) throws SQLException{
+
+		try(var connection = getConnection()){
+			new MigrationStrategy(connection).executeMigration();
+		}
+
 	}
 
 }
