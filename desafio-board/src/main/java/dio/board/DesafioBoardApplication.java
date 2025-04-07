0cbcbb7a2ac70c506e84ccaee499e686c2ae6968
Lucas Honorio <lucashonorioo@hotmail.com>
2025-04-07 16:50:34 -0300

    Projeto Criado

---
+++ desafio-board/src/main/java/dio/board/DesafioBoardApplication.java
@@ -0,0 +1,13 @@
+package dio.board;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioBoardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioBoardApplication.class, args);
+	}
+
+}
